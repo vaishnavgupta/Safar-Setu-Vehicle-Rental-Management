9e3f47f1d5669bcc8f2590f2a3b6f5a7b90c6ceb
vaishnavgupta <158202421+vaishnavgupta@users.noreply.github.com>
2026-03-01 16:25:13 +0530

    Backend Url Added

--- safarsetu-admin-frontend/src/api/axios.js
+++ safarsetu-admin-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 // Attaching JWT Token Automatically
